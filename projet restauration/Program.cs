b3d4289d2938d810232ce4befdfca1fd86e8d58b
marwan <marouanebenmhand2@gmail.com>
2023-11-24 21:52:18 +0100

    rename form 1

--- projet restauration/Program.cs
+++ projet restauration/Program.cs
@@ -16,7 +16,7 @@ namespace projet_restauration
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new loginpage());
         }
     }
 }
